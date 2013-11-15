55aafd733d2939f396c6a36ae8f19c3961ed2fe0
Mathieu <tan.mathieu@gmail.com>
2013-11-15 14:30:46 +0100

    Initial Commit

---
+++ littlebiggame/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  littlebiggame
+//
+//  Created by mataejoon on 11/15/13.
+//  Copyright (c) 2013 mataejoon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
